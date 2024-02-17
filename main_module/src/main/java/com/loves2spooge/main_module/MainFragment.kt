e729a917a083e78cd4223bd2599fd8fbf5d7017f
maksimkipreev <kipreevmaksim51@gmail.com>
2024-02-17 12:19:15 +0300

    Add navigation

--- main_module/src/main/java/com/loves2spooge/main_module/MainFragment.kt
+++ main_module/src/main/java/com/loves2spooge/main_module/MainFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainFragment : Fragment() {
 
@@ -12,8 +16,28 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bottomNavigationView =
+            view.findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        NavigationUI.setupWithNavController(
+            bottomNavigationView,
+            Navigation.findNavController(
+                requireActivity(),
+                com.loves2spooge.navigation.R.id.host_main
+            )
+        )
+
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                requireActivity().finish()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+    }
 }
